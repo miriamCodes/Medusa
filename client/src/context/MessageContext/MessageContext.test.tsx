d91df5b6ba2af5d4ed67a60d5de76f513e47c880
adamlebreee <adamlebreuilly@gmail.com>
2023-08-07 13:02:41 +0200

    updated mock data for test, remove e2e

--- client/src/context/MessageContext/MessageContext.test.tsx
+++ client/src/context/MessageContext/MessageContext.test.tsx
@@ -1,18 +1,54 @@
 import { render, act } from '@testing-library/react';
 import { MessageProvider, MessageContext } from './MessageContext';
+import { ChatContext } from '../ChatContext/ChatContext';
+import { ChatContextValues } from '../ChatContext/ChatContextTypes';
+import React, { ReactNode } from "react";
+import { Socket } from 'socket.io-client';
+
+jest.mock('../ChatContext/ChatContext');
+
+
+const mockSocket: Socket = Object.create(Socket.prototype);
+
+mockSocket.on = jest.fn();
+mockSocket.emit = jest.fn();
+mockSocket.off = jest.fn();
+
+let mockRoom = "";
+
+const mockSetRoom = (room: string) => {
+    mockRoom = room;
+};
+
+const mockChatContext: ChatContextValues = {
+    socket: mockSocket,
+    setRoom: mockSetRoom,
+    room: mockRoom,
+    chatrooms: [],
+    setChatrooms: jest.fn(),
+    userCount: 0,
+    setUserCount: jest.fn(),
+   
+};
 
 describe('MessageProvider', () => {
+    const Wrapper: React.FC<{ children: ReactNode }> = ({ children }) => (
+        <ChatContext.Provider value={mockChatContext}>{children}</ChatContext.Provider>
+    );
+
     it('should set the correct initial state', () => {
         let contextValues: any;
         render(
-            <MessageProvider>
-                <MessageContext.Consumer>
-                    {(context) => {
-                        contextValues = context;
-                        return null;
-                    }}
-                </MessageContext.Consumer>
-            </MessageProvider >
+            <Wrapper>
+                <MessageProvider>
+                    <MessageContext.Consumer>
+                        {(context) => {
+                            contextValues = context;
+                            return null;
+                        }}
+                    </MessageContext.Consumer>
+                </MessageProvider>
+            </Wrapper>
         );
         expect(contextValues.message).toBe('');
         expect(contextValues.messageList).toEqual([]);
@@ -21,19 +57,19 @@ describe('MessageProvider', () => {
     it('should update the message when setMessage is called', () => {
         let contextValues: any;
         render(
-            <MessageProvider>
-                <MessageContext.Consumer>
-                    {(context) => {
-                        contextValues = context;
-                        return null;
-                    }}
-                </MessageContext.Consumer>
-            </MessageProvider >
+            <Wrapper>
+                <MessageProvider>
+                    <MessageContext.Consumer>
+                        {(context) => {
+                            contextValues = context;
+                            return null;
+                        }}
+                    </MessageContext.Consumer>
+                </MessageProvider>
+            </Wrapper>
         );
 
-        act(() =>
-            contextValues.setMessage('test message'));
+        act(() => contextValues.setMessage('test message'));
         expect(contextValues.message).toBe('test message');
-
-    })
+    });
 });
